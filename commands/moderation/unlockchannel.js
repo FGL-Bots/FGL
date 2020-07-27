25c622de6e70c2d54fd0997dc3b563ae7ee0924a
Rootspring <cheesycod@outlook.com>
2020-07-28 02:03:20 +0530

    Message changes

--- commands/moderation/unlockchannel.js
+++ commands/moderation/unlockchannel.js
@@ -10,7 +10,7 @@ module.exports.run = (client, message, args, level, Discord) => {
       if (channel.id !== message.channel.id) {
         client.success(message.channel, 'Channel Unlocked!', 'Members can now send messages in that channel!');
       }
-      channel.send("**Channel Unlocked**!\nWe've determined that it's safe to lift channel lock precautions and allow everyone to send messages again, yes yes!");
+      channel.send("**Channel Unlocked**!\nThis channel has been unlocked. You can tyoe again now!");
     })
     .catch((error) => client.error(message.channel, 'Channel Unlock Failed!', `The channel failed to be unlocked because: \`${error}\``));
 };
