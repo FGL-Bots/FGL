e363fa89e1e33fc83e64b3e10a930ed066853065
Rootspring <cheesycod@outlook.com>
2020-07-29 15:07:21 +0530

    Fix apply

--- commands/misc/apply.js
+++ commands/misc/apply.js
@@ -1,8 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 module.exports.run = async (client, message, args, level, Discord) => {
+function cleanArray(actual) {
+  var newArray = new Array();
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i]) {
+      newArray.push(actual[i]);
+    }
+  }
+  return newArray;
+}
   const dmChannel = await message.member.createDM();
   const questions = [
-    "Just for confirmation, please retype your tag.", 
     "What is your timezone?",
     "How old are you right now?",                  
     "Why do you want to be a moderator?", 
@@ -10,7 +18,7 @@ module.exports.run = async (client, message, args, level, Discord) => {
     "If someone is abusing their role, what do you do?",
     "What do you do during a staff disagreement?",
     "What do you do in an uncomfortable situation?",
-    "Have you moderated any servers before? If yes, please type them here."            
+    "Have you moderated any servers before? If yes, please type them here."      
   ];                                     
   const filter = m => m.channel.type === 'dm' || m.author.bot === 0;
   const collector = dmChannel.createMessageCollector(filter, { });
@@ -18,17 +26,21 @@ module.exports.run = async (client, message, args, level, Discord) => {
   pane = 0;
   var ans = [];
   var stopped = 0;
-  dmChannel.send("Hello, to begin the application, please type tag (username#descriminator). If you wish to stop the application at any time, please type stop.");
+  var uwarned = 0;
+  var begun = 0;
+  dmChannel.send(`Hi there. Welcome to FGL Staff Applications. If you wish to stop the application at any time, please type stop. To start your application, please type your username#descriminator and hit enter.\n\n`);
   collector.on('collect', m => {
     if(m.author.bot) {
       return;
     }
     if(pane === questions.length) {
+      ans[pane] = m.content;
+      console.log(ans, pane);
       dmChannel.send("Are you ready to submit your application? If not, please type stop and reapply. Otherwise, type submit.");
       pane++;
       return;
     }
-    if(m.content.toLowerCase() === 'stop') {
+    if(m.content.toLowerCase() === 'stop' || uwarned === 1) {
       dmChannel.send("Stopped your application. Thank you and have a good day!");
       stopped = 1;
       collector.stop();
@@ -41,17 +53,24 @@ module.exports.run = async (client, message, args, level, Discord) => {
         return;
       }
       else {
-        dmChannel.send("Invalid response. Closing application...");
-        collector.stop();
+        dmChannel.send("Invalid response. Please type stop to close the application and type submit to submit it. Note that continuing to give an invalid response will stop your application");
+        uwarned = 1;
         return;
       }
     }
-    ans[pane] = m.content;
-    dmChannel.send(questions[pane]);
-    pane++;
+    if(pane === 0) {
+    	dmChannel.send(`**${questions[pane]}**`);
+    	pane++;
+    }
+    else {
+    	ans[pane] = m.content;
+    	dmChannel.send(`**${questions[pane]}**`);
+    	pane++;
+	}
   });
 
   collector.on('end', collected => {
+    ans.splice(0, 1);
     var msg = `__**Staff application for ${message.author.tag}**__\n`;
     var channel = client.channels.cache.get(client.config.applyChannel);
     for(var i = 0; i < questions.length; i++) {
